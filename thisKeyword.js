7c371019589f80b560cd7bcd41e117840d4f1654
dh_bgcuxj <webtekmasters@gmail.com>
2021-01-13 23:01:02 +0100

    added exercise to thisKeyword.js file

--- thisKeyword.js
+++ thisKeyword.js
@@ -14,3 +14,20 @@ const student = {
 student.class();// Output = Ade is 26 years Old! 
 
 // The keyword this, is that simple.
+
+// Additional Example
+
+// Define an object called <code>hen</code>. 
+// It should have three properties: <code>name</code> & should be set to 'Helen', 
+// <code>eggCount</code>, should be set to 0 and 
+// <code>layAnEgg</code>,  should be a method which increments the value of <code>eggCount</code> 
+// by 1 and returns the string "EGG".  You'll need to use <code>this</code>.
+// 
+const hen = {
+    name: "Helen",
+    eggCount: 0,
+    layAnEgg(){
+        this.eggCount+=1;
+        return "EGG";
+    }
+};
