5e4bb8fd641a3ce2392a277a717a6058177a3e5e
dh_bgcuxj <webtekmasters@gmail.com>
2021-01-16 22:57:54 +0100

    Added a note of Hashnode

---
+++ hashnodeNoteBlog.js
@@ -0,0 +1,18 @@
+// Refactoring a code from an Hashnode Article.
+
+const num = 3;
+function multiplyBy2 (inputNumber){
+const result = inputNumber*2;
+return result;
+}
+const output = multiplyBy2(num);
+const newOutput = multiplyBy2(10);
+
+
+const num2 = 3;
+const multiplyBy2Again = function (inputNumber) {
+    return inputNumber*2;
+};
+
+
+// multiplyBy2Again(10) Output = 20, and multiplyBy2Again(3) Output = 6
