7b60c77a45791770920b224d365a8e97892ea0de
dh_bgcuxj <webtekmasters@gmail.com>
2021-01-18 14:25:53 +0100

    Added some more note to setIntervals

--- 13setInterval.js
+++ 13setInterval.js
@@ -1,8 +1,15 @@
 // setInterval and setTimeout is a way of pausing or setting an interval 
-// for a command to fire as set in the timing of the function.
+// for a command to fire as set in the timing(In Milli Seconds) of the function.
 
 
 //setInterval repeats the process of the passed function for a specified period of set Intervals. It isn't commonly used.
+
+setInterval(() => {
+    console.log(`Hello Great Developer!!! This is setInterval `);
+}, 3000);
+
+// The above code would run at the intervals of 3 Seconds.
+// To have full control, it's best to assign it a variable or an ID.
 
 const id = setInterval(() => {
     console.log(`Hello Great Developer!!! This is setInterval `);
