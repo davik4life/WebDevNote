ec27181935331a5a45536bb5795d40e1b1ec7557
dh_bgcuxj <webtekmasters@gmail.com>
2021-01-20 23:09:34 +0100

    update on my note

--- assignment.js
+++ assignment.js
@@ -1,1 +1,5 @@
 // Couldn't get any of the Algorithms to work. I have decided to commit this progress that I would learn Data Structure.
+
+
+//Update, I hae bought the Data Structure and Algorithm course by Cold Steele onUdemy and would start learning that 
+//simutaneously while I continue to code everyday to practice and become familiar with the syntax and how things work generally.
