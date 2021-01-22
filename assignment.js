76f77ba72758d05698af1171f9cb54b2bb385c2f
dh_bgcuxj <webtekmasters@gmail.com>
2021-01-22 23:34:09 +0100

    Some notes

--- assignment.js
+++ assignment.js
@@ -3,3 +3,6 @@
 
 //Update, I hae bought the Data Structure and Algorithm course by Cold Steele onUdemy and would start learning that 
 //simutaneously while I continue to code everyday to practice and become familiar with the syntax and how things work generally.
+
+// Today was a very busy day for me, took my Baby to the hospital for check up. Didn't have any code reveiws today,
+// but would definintely do some exptensive reminder of the program tomorrow and then review my study.
