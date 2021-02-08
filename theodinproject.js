f8959fda46196dbe64b7535cf78689807aa9d932
Victor Adeshile <davik4life@gmail.com>
2021-02-08 23:54:17 +0100

    Solved the entire coding challengies

--- theodinproject.js
+++ theodinproject.js
@@ -1,2 +1,30 @@
 // I started the OdinProject The First day of February, 2021 (1/2/2021) because 
 // I wanted to learn and contribute to helping other people grow their dev skills.
+
+
+function add7(num){
+  return num + 7;
+}
+
+function multiply(num, num2){
+  return num * num2;
+}
+
+function capitalize (word){
+  let firstLetter = word.charAt(0).toUpperCase();
+  let restLetter = word.slice(1);
+  return firstLetter + restLetter;
+}
+
+function lastLetter (letter){
+  // pop the last letter.
+  let theLastLetter = letter.split('').pop();
+  return theLastLetter;
+}
+
+lastLetter("Victor");
+
+capitalize("victor");
+multiply(3,5);
+
+add7(7);
